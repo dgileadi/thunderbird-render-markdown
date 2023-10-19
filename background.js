bec9eaf10fa2a801d7bce58451b20b6779a411b2
David Gileadi <gileadis@gmail.com>
2023-10-19 16:05:14 -0700

    Add context menu item to toggle markdown rendering

--- background.js
+++ background.js
@@ -5,10 +5,12 @@ const markdownPatterns = [
 ];
 const textPlainPattern = /^text\/plain\b/i;
 const defaultDetectScope = 'all-plain-text';
-var detectScope = defaultDetectScope;
+let detectScope = defaultDetectScope;
+let isMarkdownMessage = false;
+let showingMarkdown = false;
 
 function shouldDisplayMarkdown(contentType) {
-  var patterns =
+  let patterns =
     detectScope === defaultDetectScope ? [textPlainPattern] : markdownPatterns;
   return patterns.some((pattern) => pattern.test(contentType));
 }
@@ -31,26 +33,50 @@ function getPlainText(messagePart) {
 }
 
 async function detectMarkdownMessage(tabId) {
+  isMarkdownMessage = false;
+  showingMarkdown = false;
+
   let message = await messenger.messageDisplay.getDisplayedMessage(tabId);
   let full = await messenger.messages.getFull(message.id);
 
   if (shouldDisplayMarkdown(full.headers['content-type'])) {
     const text = getPlainText(full);
     if (text) {
-      messenger.tabs.sendMessage(tabId, {
-        command: 'renderMarkdown',
-        text: text,
-      });
+      sendRenderMarkdownCommand(tabId, text);
+
+      isMarkdownMessage = true;
+      showingMarkdown = true;
     }
+  }
+
+  updateContextMenuItem();
+}
+
+function sendRenderMarkdownCommand(tabId, text) {
+  messenger.tabs.sendMessage(tabId, {
+    command: 'renderMarkdown',
+    text: text,
+    as: showingMarkdown ? 'plain' : 'markdown'
+  });
+}
+
+async function updateContextMenuItem() {
+  if (contextMenuId) {
+    messenger.menus.update(contextMenuId, {
+      title: showingMarkdown ? 'Show as Plain Text' : 'Show as Markdown',
+      visible: isMarkdownMessage,
+    });
   }
 }
 
+// When the page sends us a command (when it loaded), detect markdown
 browser.runtime.onMessage.addListener((data, sender) => {
   if (data.command === 'detectMarkdownMessage') {
     detectMarkdownMessage(sender.tab.id);
   }
 });
 
+// Listen to option changes for our detectScope option
 browser.storage.onChanged.addListener((changes) => {
   if (!!changes.scope) {
     detectScope = changes.scope.newValue || defaultDetectScope;
@@ -60,6 +86,7 @@ browser.storage.sync.get('scope').then((res) => {
   detectScope = res.scope || defaultDetectScope;
 });
 
+// Inject markdown rendering scripts and CSS
 browser.messageDisplayScripts.register({
   js: [
     { file: 'marked.min.js' },
@@ -67,4 +94,20 @@ browser.messageDisplayScripts.register({
     { file: 'initial.js' },
   ],
   css: [{ file: 'css/markdown.css' }],
+});
+
+// Add context menu item
+const contextMenuId = messenger.menus.create({
+  title: 'Show as Plain Text',
+  contexts: ['page', 'frame'],
+  visible: false,
+});
+
+// Register a listener for the context menu click
+messenger.menus.onClicked.addListener(async (info, tab) => {
+  if (info.menuItemId == contextMenuId) {
+    sendRenderMarkdownCommand(tab.id, null);
+    showingMarkdown = !showingMarkdown;
+    updateContextMenuItem();
+  }
 });
--- initial.js
+++ initial.js
@@ -1,13 +1,38 @@
 browser.runtime.onMessage.addListener((data) => {
   if (data.command === 'renderMarkdown') {
-    renderMarkdown(data.text);
+    if (data.text) {
+      renderMarkdown(data.text);
+      // displayDebugHTML();
+    }
+    displayMarkdown(data.as === 'markdown');
   }
 });
 
 function renderMarkdown(text) {
-  var parent = document.querySelector('body > div');
-  parent.innerHTML = DOMPurify.sanitize(marked.parse(text));
-  parent.classList.add('markdown');
+  const plaintextParent = document.querySelector('body > div');
+  const style = plaintextParent.getAttribute('style');
+  const markdownHTML = DOMPurify.sanitize(marked.parse(text));
+
+  plaintextParent.insertAdjacentHTML('afterend',
+    `<div class="markdown tb-message-markdown" style="${style}">${markdownHTML}</div>`);
+  const markdownParent = document.querySelector('.tb-message-markdown');
+
+  plaintextParent.classList.forEach(cls => markdownParent.classList.add(cls));
+  plaintextParent.classList.add('tb-message-plaintext');
+
+  plaintextParent.hidden = true;
+}
+
+function displayMarkdown(on) {
+  const plaintextParent = document.querySelector('.tb-message-plaintext');
+  const markdownParent = document.querySelector('.tb-message-markdown');
+  plaintextParent.hidden = on;
+  markdownParent.hidden = !on;
+}
+
+function displayDebugHTML() {
+  const body = document.querySelector('body');
+  body.append(body.innerHTML);
 }
 
 browser.runtime.sendMessage({ command: 'detectMarkdownMessage' });
